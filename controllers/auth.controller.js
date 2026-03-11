da350a8fedc66d0ab63b431f213e0cded8e195a3
Muhammad ayub <muhammadayubmrk@gmail.com>
2026-03-11 07:06:25 +0500

    auth issue fixed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -26,7 +26,12 @@ const login = async (req, res) => {
             process.env.jwt_secret
         );
 
-        res.cookie("token", token, { httpOnly: true });
+      res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,       // HTTPS only
+    sameSite: "none",   // MUST be string "none"
+    maxAge: 7*24*60*60*1000
+});
         return res.status(200).json({ message: "Logged In!" });
 
     } catch (error) {
