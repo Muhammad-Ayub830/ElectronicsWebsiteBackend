c3e97626fe2fdecf18c9ada13b2283da1a29e3ad
Muhammad ayub <muhammadayubmrk@gmail.com>
2026-03-17 10:50:13 +0500

    recheck

--- utilites/db.utility.js
+++ utilites/db.utility.js
@@ -1,14 +1,11 @@
-const mongoose = require("mongoose");
-
+const mongoose = require("mongoose")
 const ConnectToDatabase = async (url) => {
   try {
-    await mongoose.connect(url, {
-      serverSelectionTimeoutMS: 10000, // prevents long hangs
-    });
-    console.log("database connected Successfully!");
+    await mongoose.connect(url)
+    console.log('database connected Successfully!')
   } catch (error) {
-    console.log("DB ERROR:", error);
+    console.log(error)
   }
-};
+}
 
-module.exports = ConnectToDatabase;
+module.exports = ConnectToDatabase
